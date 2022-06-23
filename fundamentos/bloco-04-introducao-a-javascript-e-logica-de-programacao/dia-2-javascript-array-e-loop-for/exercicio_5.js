9df5cf9d550fe3ae582afad0c2ff35ab672597aa
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-06-22 23:48:34 -0300

    Exercicio 5 resolvido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_5.js
@@ -0,0 +1,13 @@
+// 5- 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero = 0;
+
+for(i = 0; i < numbers.length; i += 1){
+    if(numbers[i] > maiorNumero){
+        maiorNumero = (numbers[i]);
+    }
+}
+
+console.log(maiorNumero);
