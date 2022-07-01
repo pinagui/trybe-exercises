5d7576836335711a576b3e5265297b4e1d1cbb2f
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-07-01 01:42:54 -0300

    Exercício 3 concluído

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte_1/exercicio_3.js
@@ -0,0 +1,21 @@
+//  3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+/*
+personagem
+origem
+nota
+recorrente
+*/
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+info.recorrente = 'Sim';
+
+for(let i in info){
+    console.log(i);
+}
+
