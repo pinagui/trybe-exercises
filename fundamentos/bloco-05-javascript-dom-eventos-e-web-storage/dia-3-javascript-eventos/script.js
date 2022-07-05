cd9929253e7cd7e51baa4b45c406d971ab6c7e3c
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-07-05 01:48:49 -0300

    Exercício 2 concluído

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,46 +1,66 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   };
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+};
 
-  /* 1- Crie um calendário dinamicamente.
-    O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+createDaysOfTheWeek();
 
-    Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira
+// Escreva seu código abaixo.
 
-    A tag <ul> deve conter o id 'days'; 
-      
-    Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li> 
+/* 1- Crie um calendário dinamicamente.
+  O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
 
-    Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li> 
+  Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira
 
-    Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> 
-  */
+  A tag <ul> deve conter o id 'days'; 
+    
+  Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li> 
 
-  let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  let ulList = document.getElementById('days');
+  Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li> 
 
-  for(let day of decemberDaysList){
-    let dayList = document.createElement('li');
-    dayList.innerText = day;
-    dayList.className = 'day';
-    if(dayList.innerText === '24' || dayList.innerText === '25' || dayList.innerText === '31'){
-        dayList.classList.add('holiday');
-    }
-    if(dayList.innerText === '4' || dayList.innerText === '11' || dayList.innerText === '18' || dayList.innerText === '25'){
-        dayList.classList.add('friday');
-    }
-    ulList.appendChild(dayList);
+  Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li> 
+*/
+
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let ulList = document.getElementById('days');
+
+for (let day of decemberDaysList) {
+  let dayList = document.createElement('li');
+  dayList.innerText = day;
+  dayList.className = 'day';
+  if (dayList.innerText === '24' || dayList.innerText === '25' || dayList.innerText === '31') {
+    dayList.classList.add('holiday');
   }
+  if (dayList.innerText === '4' || dayList.innerText === '11' || dayList.innerText === '18' || dayList.innerText === '25') {
+    dayList.classList.add('friday');
+  }
+  ulList.appendChild(dayList);
+}
+
+/* 2-  Implemente uma função que crie dinamicamente um botão com o nome "Feriados". 
+
+  Sua função deve receber um parâmetro com a string 'Feriados' 
+
+  Adicione a este botão a ID "btn-holiday" 
+
+  Adicione este botão como filho/filha da tag <div> com classe "buttons-container" 
+*/
+
+function createHolidayButton(buttonName) {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
+  newButton.id = 'btn-holiday';
+  newButton.innerHTML = buttonName;
+  
+  buttonContainer.appendChild(newButton);
+}
+
+createHolidayButton('Feriados');
