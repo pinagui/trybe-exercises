1cf4d70f1b202a87f9f6d21ce2975afed28c0110
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-07-14 23:35:30 -0300

    Exercício 2 concluído

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -33,3 +33,17 @@
     testingScope(true);
 
 
+
+    // 🚀 Crie uma função que retorne um array em ordem crescente.
+
+    // Crie uma função que faça com que o array oddsAndEvens fique na ordem crescente;
+    // Utilize o console.log e template literals para retornar a frase: 'Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!'.
+    
+    //     Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+
+    // Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números. 
+
+    const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+    oddsAndEvens.sort((a, b) => a - b);
+    
+    console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
