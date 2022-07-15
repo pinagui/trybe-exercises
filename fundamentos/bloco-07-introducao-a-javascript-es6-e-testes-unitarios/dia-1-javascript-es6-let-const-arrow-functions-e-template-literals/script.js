4cedfe41d0a2e45077695101c3e33400e2f0101f
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-07-15 00:21:28 -0300

    Exercício 2.1 concluído

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -47,3 +47,23 @@
     oddsAndEvens.sort((a, b) => a - b);
     
     console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+    // Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let, const, arrow functions, template literals e ternary operator.
+
+    // Crie uma função que receba um número e retorne seu fatorial.
+
+    // Na matemática, o fatorial de um número inteiro e positivo N, representado por N!, é o produto de todos os seus antecessores até o número um. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+    // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+
+const fatorial = (numb) => {
+
+  let resultado = '1'; 
+
+  for(let i = 2; i <= numb; i += 1){
+    resultado *= i;
+  }
+  return resultado;
+}
+
+console.log(fatorial(4));
